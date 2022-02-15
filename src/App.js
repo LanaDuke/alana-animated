876813b4101d93f9c97b7c3e0637fcfe47121219
Alana Leduc <alanaleduc@gmail.com>
2022-02-15 12:08:09 -0600

    Finished tutorial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 
-import { createTheme, ThemeProvider } from '@material-ui/core/styles';
-import CustomBtn from './components/CustomBtn'
+import { createTheme, ThemeProvider, makeStyles } from '@material-ui/core/styles';
+import {Typography} from '@material-ui/core';
+import Footer from './components/Footer';
+import NavBar from './components/NavBar'
+import Grid from './components/Grid'
 import './App.css';
+
+import SecurityIcon from '@material-ui/icons/Security';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import TrendingUpIcon from '@material-ui/icons/TrendingUp';
+import ImportExportIcon from '@material-ui/icons/ImportExport';
+import ComputerIcon from '@material-ui/icons/Computer';
+import HttpIcon from '@material-ui/icons/Http';
 
 const theme = createTheme ({
   palette: {
@@ -28,10 +38,56 @@ const theme = createTheme ({
   },
 });
 
+const styles = makeStyles({
+  wrapper: {
+    width: "65%",
+    margin: "auto",
+    textAlign: "center",
+  },
+  bigSpace: {
+    marginTop: "5rem"
+  },
+  littleSpace: {
+    marginTop: "2.5rem"
+  },
+  grid: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexWrap: "wrap",
+  },
+})
+
 function App() {
+  const classes = styles();
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
+        <NavBar/>
+        <div className={classes.wrapper}>
+          <Typography variant="h4" className={classes.bigSpace} color="primary">
+            At Rocket.io we are passionate aboout software
+          </Typography>
+          <Typography variant="h5" className={classes.littleSpace} color="primary">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sodales congue tri....... Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+          </Typography>
+        </div>
+
+
+        <div className={`${classes.grid} ${classes.bigSpace}`}>
+          <Grid icon={<SecurityIcon style={{fill: "#4360A6", height:"125", width:"125"}}/>} title="Secure" btnTitle="Show Me More" />
+          <Grid icon={<EventNoteIcon style={{fill: "#449A76", height:"125", width:"125"}}/>} title="Reliable" btnTitle="Show Me More" />
+          <Grid icon={<TrendingUpIcon style={{fill: "#D05B2D", height:"125", width:"125"}}/>} title="Performant" btnTitle="Show Me More" />
+        </div>
+
+        <div className={`${classes.grid} ${classes.littleSpace}`}>
+          <Grid icon={<ImportExportIcon style={{fill: "#4360A6", height:"125", width:"125"}}/>} title="Modular" btnTitle="Show Me More" />
+          <Grid icon={<ComputerIcon style={{fill: "#449A76", height:"125", width:"125"}}/>} title="Multi-Platform" btnTitle="Show Me More" />
+          <Grid icon={<HttpIcon style={{fill: "#D05B2D", height:"125", width:"125"}}/>} title="Connected" btnTitle="Show Me More" />
+        </div>
+        <div className={classes.bigSpace}>
+          <Footer/>
+        </div>
 
       </ThemeProvider>
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,39 @@ import CustomBtn from './CustomBtn'
 import logo from '../logo.svg'
 import logoMobile from '../logoMobile.svg'
 import {Toolbar, Typography} from '@material-ui/core'
-import {makeStyles} from ";@material-ui/core/styles";
+import {makeStyles} from "@material-ui/core/styles";
 
+const styles = makeStyles({
+    bar:{
+        paddingTop: "1.15rem",
+        backgroundColor: "#fff",
+        ['@media (max-width:780px)']: {
+            flexDirection: "column"
+        }
+    },
+    logo: {
+        width: "15%",
+        ['@media (max-width:780px)']: {
+            display: "none"
+        }
+    },
+    logoMobile: {
+        width: "100%",
+        display: "none",
+        ['@media (max-width:780px)']: {
+            diaplay: "inline-block"
+        }
+    },
+    menuItem: {
+        cursor: "pointer",
+        flexGrow: 1,
+        "&:hover": {
+            color: "#4f25c8"
+        },
+        ['@media (max-width:780px']: {
+            paddingBottom: "1rem" }
+        }
+})
 
 export default function NavBar() {
     const classes = styles()
